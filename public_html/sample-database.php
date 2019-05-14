255d5257985eb0c3647c6ffb430944f1c2518841
bibincatchme <42738880+bibincatchme@users.noreply.github.com>
2019-05-14 16:04:42 +0530

    Create sample-database.php

---
+++ public_html/sample-database.php
@@ -0,0 +1,18 @@
+$active_group = 'default';
+$active_record = TRUE;
+
+$db['default']['hostname'] = 'hiltonrds.cvb3x10otois.us-east-1.rds.amazonaws.com';
+$db['default']['username'] = 'username';
+$db['default']['password'] = 'password';
+$db['default']['database'] = 'database';
+$db['default']['dbdriver'] = 'mysql';
+$db['default']['dbprefix'] = '';
+$db['default']['pconnect'] = TRUE;
+$db['default']['db_debug'] = TRUE;
+$db['default']['cache_on'] = FALSE;
+$db['default']['cachedir'] = '';
+$db['default']['char_set'] = 'utf8';
+$db['default']['dbcollat'] = 'utf8_general_ci';
+$db['default']['swap_pre'] = '';
+$db['default']['autoinit'] = TRUE;
+$db['default']['stricton'] = FALSE;
