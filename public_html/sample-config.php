7fbdc1a40deab2c3ede47c1d610aeca1b6fe645f
bibincatchme <42738880+bibincatchme@users.noreply.github.com>
2019-05-14 16:05:30 +0530

    Create sample-config.php

---
+++ public_html/sample-config.php
@@ -0,0 +1,30 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+  
+/*
+|--------------------------------------------------------------------------
+| Base Site URL
+|--------------------------------------------------------------------------
+|
+| URL to your CodeIgniter root. Typically this will be your base URL,
+| WITH a trailing slash:
+|
+|       http://example.com/
+|
+| If this is not set then CodeIgniter will try to guess the protocol, domain
+| and path to your installation. However, you should always configure this
+| explicitly and never rely on auto-guessing, especially in production
+| environments.
+|
+*/
+$config['base_url']     = '';
+
+/*
+|--------------------------------------------------------------------------
+| Index File
+|--------------------------------------------------------------------------
+|
+| Typically this will be your index.php file, unless you've renamed it to
+| something else. If you are using mod_rewrite to remove the page set this
+| variable so that it is blank.
+|
+*/
